81c7e1787c87cb675bbed8af4d5813794e11e27b
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2017-08-07 12:54:52 +0200

    Add basic draft

--- find-wip.sh
+++ find-wip.sh
@@ -1,1 +1,13 @@
+git_status=$(mktemp)
+temp=$(mktemp)
 
+find "$1" -iname ".git" -type d >> $temp
+
+while read -r line; do
+    cd "$line/.."
+    pwd >> $git_status
+    git status|grep "Your branch is ahead" >> $git_status
+    cd - >> $git_status
+done < $temp
+
+cat $git_status |grep "ahead" -B1
